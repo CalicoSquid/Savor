90f51e4bd0147d3c5398accc687d810032e56989
Caleb Mow <immensedrama@gmail.com>
2023-08-07 22:26:59 +0200

    changed http

--- src/Utilities/config.js
+++ src/Utilities/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    baseURL: "http://server-indol-rho.vercel.app/api", // Replace with your local API URL
+    baseURL: "https://server-indol-rho.vercel.app/api", // Replace with your local API URL
     //"http://endearing-hamster-3c0be8.netlify.app/api"
     //"http://192.168.1.109:8000/api"
     //"https://harmonious-douhua-da2a73.netlify.app/api"
