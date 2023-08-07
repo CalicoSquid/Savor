40b68c567107eb1c4b42a64e4c22057a0e5dee28
Caleb Mow <immensedrama@gmail.com>
2023-08-07 18:25:17 +0200

    Changes to http://

--- src/Utilities/config.js
+++ src/Utilities/config.js
@@ -1,10 +1,10 @@
 // config.js
 const config = {
-    baseURL: "http://server-calicosquid.vercel.app/api", // Replace with your local API URL
+    baseURL: "https://server-calicosquid.vercel.app/api", // Replace with your local API URL
     //"http://endearing-hamster-3c0be8.netlify.app/api"
     //"http://192.168.1.109:8000/api"
-    //"https://harmonious-douhua-da2a73.netlify.app"
-    //"https://server-indol-rho.vercel.app"
+    //"https://harmonious-douhua-da2a73.netlify.app/api"
+    //"https://server-indol-rho.vercel.app/api"
   };
   
   export default config;
