5eab8b2bcc097441406f743212915104b6f401fc
Caleb Mow <immensedrama@gmail.com>
2023-08-07 14:27:44 +0200

    Changed config

--- src/Utilities/config.js
+++ src/Utilities/config.js
@@ -1,6 +1,8 @@
 // config.js
 const config = {
-    baseURL: "http://192.168.1.109:8000/api", // Replace with your local API URL
+    baseURL: "http://endearing-hamster-3c0be8.netlify.app/api", // Replace with your local API URL
+    //endearing-hamster-3c0be8.netlify.app
+    //"http://192.168.1.109:8000/api"
   };
   
   export default config;
