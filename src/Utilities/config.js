21c15c9ccb970b4ec6f32cc27afffaa05471c09c
Caleb Mow <immensedrama@gmail.com>
2023-08-07 18:00:16 +0200

    Added favicon

--- src/Utilities/config.js
+++ src/Utilities/config.js
@@ -1,9 +1,10 @@
 // config.js
 const config = {
-    baseURL: "https://harmonious-douhua-da2a73.netlify.app/api", // Replace with your local API URL
+    baseURL: "https://server-indol-rho.vercel.app", // Replace with your local API URL
     //"http://endearing-hamster-3c0be8.netlify.app/api"
     //"http://192.168.1.109:8000/api"
     //"https://harmonious-douhua-da2a73.netlify.app"
+    //"https://server-indol-rho.vercel.app"
   };
   
   export default config;
